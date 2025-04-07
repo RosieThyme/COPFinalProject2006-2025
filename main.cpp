e20806b2056763cc8cf21380428cd7f28cbd3c21
damien <damienprovost98@gmail.com>
2025-04-07 12:46:07 -0400

    This version doesnt show correct answer choices and all the "right" answers are option 1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
-//
-//dsakjdha
-//
-#include "QuizLoop.h"
-#include <iostream>
-#include <limits>
-#include <vector>
+#include <QApplication>
+#include "quizwidget.h"
 
-int main()
- {
-    std::vector<std::string> QSet =  {"Which of these best describes a namespace",
-        "In a Function Invocation an epilogue is",
-    "the act of compiling function invocations in order to optimize code is called",
-        "which of these options is considered a macro",
-    "how would you dereference the pointer variable pdata"};
-    std::vector<std::string> AnswerOptions = {"\nA.a file that can be open using the fopen fucntion\nB.a library containing functions that can be accessed through the name of the library\nC.a built-in library that contains the standard classes and functions\nD.a technique used to shorten variable/function name\n"};
-    std::vector<char> QSetAnswers ={'b','a'};
-
-    QuizQuestion quizStruct(QSet,QSetAnswers);
-    quizStruct.GameLoop();
-    return 0;
-
- }
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);  // Initialize Qt application
+    QuizWidget quiz;  // Create quiz widget
+    quiz.show();  // Show the quiz window
+    return app.exec();  // Start the event loop
+}
