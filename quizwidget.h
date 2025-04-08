1906537305d0a7c6baa718e77352149f68ab1c9f
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2025-04-08 15:57:17 -0400

    program now loops through the correctAnswer vector

--- quizwidget.h
+++ quizwidget.h
@@ -19,18 +19,18 @@ public:
     QuizWidget(QWidget *parent = nullptr)
         : QWidget(parent),
           quizLoop({
-              {"Which of these best describes a namespace?","1.a file that can be open using the fopen fucntion,\n2.a library containing functions that can be accessed through the name of the library\n3.a built-in library that contains the standard classes and functions\n4.a technique used to shorten variable/function name"},
-{"In a function invocation, an epilogue is?","1.What a function executes last\n2.what happens after compling the code\n3.what occurs at the end of the main function\n4.the start of a function"},
-{"The act of compiling function invocations in order to optimize code is called?","A.function call\nB.argument vector\nC.function parameter\nD.inline function"},
-{"Which of these options is considered a macro?", "1.int data\n2.class data\n3.#define data\n4.struct data"},
-{"How would you dereference the pointer variable `data`?","1.&pdata\n2.this->pdata\n3.*pdata\n4.pdata"},
-              {"What is the purpose of the `virtual` keyword in C++?"},
-              {"Which type of memory allocation is handled by `new`?"},
-              {"What does the `friend` keyword do in C++?"},
-              {"Which operator is used to access members of a class through a pointer?"},
-              {"How do you prevent a class from being inherited in C++?"}
+              {"Which of these best describes a namespace?","1.a file that can be open using the fopen function,\n2.a library containing functions that can be accessed through the name of the library\n3.a built-in library that contains the standard classes and functions\n4.a technique used to shorten variable/function name"},
+            {"In a function invocation, an epilogue is?","1.What a function executes last\n2.what happens after compling the code\n3.what occurs at the end of the main function\n4.the start of a function"},
+            {"The act of compiling function invocations in order to optimize code is called?","A.function call\nB.argument vector\nC.function parameter\nD.inline function"},
+            {"Which of these options is considered a macro?", "1.int data\n2.class data\n3.#define data\n4.struct data"},
+            {"How would you dereference the pointer variable `data`?","1.&pdata\n2.this->pdata\n3.*pdata\n4.pdata"},
+              {"What is the purpose of the `virtual` keyword in C++?","1.to create a GUI in the program\n2.allows the programmer to create new functions from the base class\n3.to hide the implementation of the program from the user\n4. A method of error handling"},
+              {"Which type of memory allocation is handled by `new`?","1.stack allocation\n2.heap allocation\n3.dynamic memory allocation\n4.static memory allocation"},
+              {"What does the `friend` keyword do in C++?","1.creates a new function of the class\n2.inherits the class\n3.a form of a parent class in c++\n4.allows access to a class's protected and private members"},
+              {"Which operator is used to access members of a class through a pointer?","1.->\n2.&\n3.^\n4.\\n"},
+              {"How do you prevent a class from being inherited in C++?","1.private\n2.Final\n3.final\n4.protected"}
           },
-          {'1', '3', '2', '4', '1', '2', '3', '4', '1', '3'}),
+          {'2', '1', '4', '3', '3', '2', '3', '4', '1', '3'}),
           currentQuestionIndex(0) {
 
         setWindowTitle("C++ Quiz Game");
@@ -62,7 +62,9 @@ private:
     void displayQuestion() {
         if (currentQuestionIndex < quizLoop.questionList.size()) {
             pquestionLabel->setText(QString::fromStdString(quizLoop.questionList[currentQuestionIndex][0]));
-            poptionsLabel->setText(QString::fromStdString(quizLoop.questionList[currentQuestionIndex][1]);
+            poptionsLabel->setText(QString::fromStdString(quizLoop.questionList[currentQuestionIndex][1]));
+            quizLoop.correctAnswer[currentQuestionIndex];
+
             pscoreLabel->setText("Score: " + QString::number(quizLoop.score));
         } else {
             pquestionLabel->setText("Congrats! You have finished the quiz!");
